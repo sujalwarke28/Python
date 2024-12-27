c50b5ab9b06e752667edcd8547fe2aa8fbd99ad3
Sujal Warke <2024.sujalw@isu.ac.in>
2024-12-27 20:58:43 +0530

    Update (1)good_morning.py

--- Chapter 3 (Strings)/Problem set 3/(1)good_morning.py
+++ Chapter 3 (Strings)/Problem set 3/(1)good_morning.py
@@ -1,9 +1,8 @@
-from tensorflow.python.ops.logging_ops import Print
-
 user = input('Enter your name: ')
 
 #methid 1
 # print('Good morning,', user)
 
+#this is a f string
 # method 2
 print(f'Good morning, {user}')
