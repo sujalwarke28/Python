663e0387ca361a8b61d863296609bd871ba98c76
Sujal Warke <2024.sujalw@isu.ac.in>
2024-12-27 21:01:45 +0530

    Add files via upload

---
+++ List and Tuples/(2)student_marks.py
@@ -0,0 +1,18 @@
+marks=[]
+
+f1=input('Marks 1: ')
+marks.append(f1)
+f2=input('Marks 2: ')
+marks.append(f2)
+f3=input('Marks 3: ')
+marks.append(f3)
+f4=input('Marks 4: ')
+marks.append(f4)
+f5=input('Marks 5: ')
+marks.append(f5)
+f6=input('Marks 6: ')
+marks.append(f6)
+
+marks.sort()
+
+print(marks)
