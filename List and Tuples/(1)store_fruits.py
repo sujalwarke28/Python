cedf442da9c52662a3cdc1ed91a95aa9e5411e49
Sujal Warke <2024.sujalw@isu.ac.in>
2024-12-27 21:01:14 +0530

    Add files via upload

---
+++ List and Tuples/(1)store_fruits.py
@@ -0,0 +1,19 @@
+fruits=[]
+
+f1=input('Fruit1: ')
+fruits.append(f1)
+f2=input('Fruit2: ')
+fruits.append(f2)
+f3=input('Fruit3: ')
+fruits.append(f3)
+f4=input('Fruit4: ')
+fruits.append(f4)
+f5=input('Fruit5: ')
+fruits.append(f5)
+f6=input('Fruit6: ')
+fruits.append(f6)
+f7=input('Fruit7: ')
+fruits.append(f7)
+
+print(fruits)
+
