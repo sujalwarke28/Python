a52f93c8a7206d0caf300e65095f6dabb34d50c1
Sujal Warke <2024.sujalw@isu.ac.in>
2024-12-27 21:07:22 +0530

    Add files via upload

---
+++ Strings/escape_seq.py
@@ -0,0 +1,12 @@
+initial_stri = 'this is a string'
+
+final_string1='this\nis\na\nstring\n'
+final_string2='this\tis\ta\tstring\t'
+final_string3='this is a \'string\''
+final_string4='this is a \"string\"'
+
+print(initial_stri)
+print(final_string1)
+print(final_string2)
+print(final_string3)
+print(final_string4)
---
+++ Strings/negative_slicing.py
@@ -0,0 +1,10 @@
+name= 'sujal'
+
+print(name[-6:-1])
+print(name[-3:-1])
+print(name[-5:-3])
+print(name[-5:])
+print(name[:-1])
+
+
+
