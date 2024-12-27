dee8db9f336849e4b3e8ada389a9cd53489a9c0b
Sujal Warke <2024.sujalw@isu.ac.in>
2024-12-27 21:02:04 +0530

    Add files via upload

---
+++ List and Tuples/(3)check_tuples.py
@@ -0,0 +1,12 @@
+a = (33, 'sujal', 28)
+
+a[2] = '21'
+
+#This error is our answer
+'''
+Traceback (most recent call last):
+  File "/Users/sujalwarke/Desktop/Python /Practice/Chapter 4/Problem set 4/(3)check_tuples.py", line 3, in <module>
+    a[2] = '21'
+    ~^^^
+TypeError: 'tuple' object does not support item assignment
+'''
