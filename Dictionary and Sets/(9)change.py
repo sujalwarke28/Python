bfc950e15bc837f264bb66e06f10077c6bf24805
Sujal Warke <2024.sujalw@isu.ac.in>
2024-12-28 23:28:16 +0530

    Add files via upload

---
+++ Dictionary and Sets/(9)change.py
@@ -0,0 +1,5 @@
+# Can you change the values inside a list which is contained in set S?
+
+s = {8, 7, 12, "Harry", [1,2]}
+
+# you will get an error, which says it is an unhashable type 'list', which means a set cannot be included in a dictionary
