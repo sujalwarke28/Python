e98f4831eb373a5afc69e60f27688226e1241fcc
Sujal Warke <2024.sujalw@isu.ac.in>
2024-12-28 23:26:40 +0530

    Add files via upload

---
+++ Dictionary and Sets/(6) fav_lang.py
@@ -0,0 +1,24 @@
+d={}
+
+# PERSON 1
+name = input('name: ')
+lang = input('lang: ')
+d.update({name: lang})  #we use 'update' to fill in new data
+
+# PERSON 2
+name = input('name: ')
+lang = input('lang: ')
+d.update({name: lang})
+
+# PERSON 3
+name = input('name: ')
+lang = input('lang: ')
+d.update({name: lang})
+
+# PERSON 4
+name = input('name: ')
+lang = input('lang: ')
+d.update({name: lang})
+
+print(d)
+print(type(d))
