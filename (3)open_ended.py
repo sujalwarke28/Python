f7b2e3ffafee923a57a98c2ff107a81474cdca00
Sujal Warke <2024.sujalw@isu.ac.in>
2024-12-28 23:23:47 +0530

    Add files via upload

---
+++ (3)open_ended.py
@@ -0,0 +1,3 @@
+# Can we have a set with 18 (int) and '18' (str) as a value in it?
+
+print('True, coz 18 is integer and "18" is string ')
---
+++ Dictionary and Sets/(1)hindi_to_english.py
@@ -0,0 +1,10 @@
+words = {
+    'achha' : 'better',
+    'ghar' : 'house',
+    'gaadi' : 'car',
+}
+
+print(words)
+
+value = words.get(input('what do you want: '))
+print(value)
