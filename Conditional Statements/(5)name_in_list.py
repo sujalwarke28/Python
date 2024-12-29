315d02067f86076d797b9e98c12d3ca652b14b30
Sujal Warke <2024.sujalw@isu.ac.in>
2024-12-29 22:44:24 +0530

    Add files via upload

---
+++ Conditional Statements/(5)name_in_list.py
@@ -0,0 +1,9 @@
+l = ['Sujal', 'Vinayak', 'Atharva', 'Aadesh', 'Rohan', 'Tansihq']
+
+name=input('name: ')
+
+if name in l:
+    print('You are on the guest list')
+
+else:
+    print('Not on the guest list......pls register')
