bcbf6724bcde3cfb3e50ec57959fbc84016b25ae
Sujal Warke <2024.sujalw@isu.ac.in>
2024-12-29 22:40:16 +0530

    Add files via upload

---
+++ Conditional Statements/condtionals.py
@@ -0,0 +1,31 @@
+#IF STATEMENTS
+'''
+num = 10
+if num < 11:
+    print('Number is smaller than 11,', num)
+'''
+
+#IF ELSE STATEMENTS
+
+'''
+x = int(input('This is your num: '))
+
+if x > 10:
+    print(f'Your number {x}, is bigger than 10')
+else:
+    print(f'your number {x}, is smaller than 10')
+'''
+# IF-ELIF-ELSE STATEMENTS
+
+'''
+x = int(input('This is your num: '))
+
+if x < 10:
+    print(f'Your number {x}, is smaller than 10')
+
+elif x>=10 and x<20:
+    print(f'your number {x} is bigger than 10 but smaller than 20')
+else:
+    print(f'Your number {x} is even bigger than 20')
+'''
+
