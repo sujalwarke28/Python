27842160f5549e85a0c3cf0e5d43a2cba1597211
Sujal Warke <2024.sujalw@isu.ac.in>
2024-12-30 22:44:33 +0530

    Add files via upload

---
+++ Loops in Python/for_loop_interation.py
@@ -0,0 +1,19 @@
+#Iteration in List
+l=[1,4,6,7,88,33,56,22,12]
+
+for i in l:
+    print (i)
+
+
+
+#iteration in tuple
+t  = (1,5,77,85,321,56)
+
+for i in t:
+    print (i)
+
+
+# iteration in string
+s = 'sujal'
+for i in s:
+    print (i)
