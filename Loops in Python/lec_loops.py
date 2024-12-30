222a738e1049f933ef95cb3072e1992ad23cdbf0
Sujal Warke <2024.sujalw@isu.ac.in>
2024-12-30 14:25:31 +0530

    Add files via upload

---
+++ Loops in Python/lec_loops.py
@@ -0,0 +1,40 @@
+# METHOD 1: defining variable and then applying loops on that (USING WHILE LOOP)
+'''num = 0
+
+while num<1001:
+    print(num)
+    num+=1
+'''
+
+
+
+#METHOD 2: USING FOR LOOP
+'''
+for i in range(1,101):
+    print(i)
+'''
+
+
+'''i = 0
+while i < 11:
+    print('Sujal')
+    i+=1
+
+for n in range(1,11):
+    print('Warke')
+'''
+
+
+l = [67, 865, 77, 28, 'Sujal', 9791, 778, 'Warke']
+i=0
+while  (i<len(l)):
+    print(l[i])
+    i+=1
+
+# l = [1, "Harry", False, "This", "Rohan"', "Shubham"', "Shubhi"]
+# i= 0
+# while (i<len(l)):
+#     print(l[i])
+#
+#     i +=1
+
