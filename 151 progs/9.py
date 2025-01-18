cf2356e000e84a962372176e08d440ae246ad41d
Sujal Warke <2024.sujalw@isu.ac.in>
2025-01-18 22:45:28 +0530

    Add files via upload

---
+++ 151 progs/9.py
@@ -0,0 +1,7 @@
+#WAP TO CALC AREA OF CIRCLE USING RADIUS AS USRINP
+
+radius = int(input("Enter radius: "))
+
+print(f"The Area of the Circle is {3.14159 * radius ** 2}")        #Area of circle = pi*r^2 (3.14 * r^2)
+
+print("Execution Completed")
